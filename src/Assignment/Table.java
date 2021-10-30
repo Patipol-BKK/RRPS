baa707add665042cc56eb698ada4307cb94d47a2
Kencer024 <KLOW024@e.ntu.edu.sg>
2021-10-30 12:15:00 +0800

    fixed reservation

--- src/Assignment/Table.java
+++ src/Assignment/Table.java
@@ -8,4 +8,8 @@ abstract class Table {
     public int getTableId(){return this.tableId;}
     public boolean getreserved(){return this.reserved;}
     public int getpax(){return this.pax;}
+
+    public void setTableId(int tableId){this.tableId = tableId;}
+
+    public void setReserved(boolean r){this.reserved = r;}
 }
