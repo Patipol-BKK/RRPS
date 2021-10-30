7dd89a49570133412d07377089b1bb346fdcd6f9
Kencer024 <KLOW024@e.ntu.edu.sg>
2021-10-30 11:30:07 +0800

    initial

---
+++ src/Assignment/Table.java
@@ -0,0 +1,11 @@
+package Assignment;
+
+abstract class Table {
+    private int tableId;
+    private boolean reserved;
+    private int pax;
+
+    public int getTableId(){return this.tableId;}
+    public boolean getreserved(){return this.reserved;}
+    public int getpax(){return this.pax;}
+}
